4c24b02d269d58b612b950abc0f0762796acd07b
lucaspolaquini <cmte.lucas@hotmail.com>
2017-09-25 21:42:57 -0300

    Authentication in WebAPI

---
+++ CoreOneAuthentication/ApiToken/Controllers/TokenController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace ApiToken.TokenController
+{
+    [Route("/token")]
+    public class ValuesController : Controller
+    {
+        [HttpPost]
+        public IActionResult Create(string username, string password)
+        {
+            if (IsValidUserAndPasswordCombination(username, password))
+            {
+                var token = GenerateToken(username);
+                //Salvar no DB
+                return new ObjectResult(token);
+
+            }
+            return BadRequest();
+        }
+
+        private bool IsValidUserAndPasswordCombination(string username, string password)
+        {
+            if (username == "lucasfpo" && password == "123")
+                return true;
+            return false; 
+        }
+
+        private string GenerateToken(string username)
+        {
+            var claims = new Claim[]
+            {
+                new Claim(ClaimTypes.Name, username),
+                new Claim(JwtRegisteredClaimNames.Nbf, new DateTimeOffset(DateTime.Now).ToUnixTimeSeconds().ToString()),
+                //Data para expirar o token neste cado, um dia
+                new Claim(JwtRegisteredClaimNames.Exp, new DateTimeOffset(DateTime.Now.AddDays(1)).ToUnixTimeSeconds().ToString()),
+            };
+
+            var token = new JwtSecurityToken(
+                new JwtHeader(new SigningCredentials(
+                    new SymmetricSecurityKey(Encoding.UTF8.GetBytes("the secret that needs to be at least 16 characeters long for HmacSha256")),
+                                             SecurityAlgorithms.HmacSha256)),
+                new JwtPayload(claims));
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
